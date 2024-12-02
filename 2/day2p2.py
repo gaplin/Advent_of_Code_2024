ff37c1d08f23b5bc409989099951492362991bde
gaplin <50521366+gaplin@users.noreply.github.com>
2024-12-02 22:36:52 +0100

    2

---
+++ 2/day2p2.py
@@ -0,0 +1,24 @@
+import re
+
+lines = open('input2.txt').read().splitlines()
+
+result = 0
+for report in lines:
+    source_nums = [int(x) for x in re.findall(r'\d+', report)]
+    for idx_to_pop in range(0, len(source_nums)):
+        nums = [x for x in source_nums]
+        nums.pop(idx_to_pop)
+
+        diff = nums[1] - nums[0]
+        if diff == 0:
+            continue
+        for i in range(1, len(nums)):
+            current_diff = nums[i] - nums[i - 1]
+            if (diff < 0 and current_diff >= 0) or (diff > 0 and current_diff <= 0) or abs(current_diff) > 3:
+                break
+        else:
+            result += 1
+            break
+
+
+print(result)
