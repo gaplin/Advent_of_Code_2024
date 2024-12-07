8686ed9454e88bc2f94e3f289f8880e339823e98
gaplin <50521366+gaplin@users.noreply.github.com>
2024-12-07 13:07:01 +0100

    More efficient solution for day 7

---
+++ 7/day7p1v2.py
@@ -0,0 +1,20 @@
+input = open('input2.txt').read().splitlines()
+
+def possible_nums(target: int, nums: list) -> bool:
+    if len(nums) == 1:
+        return target == nums[0]
+    if target > nums[-1] and possible_nums(target - nums[-1], nums[:-1]):
+        return True
+    if target % nums[-1] == 0 and possible_nums(target // nums[-1], nums[:-1]):
+        return True
+    return False
+
+result = 0
+for line in input:
+    target, rest = line.split(': ')
+    target = int(target)
+    nums = [int(x) for x in rest.split(' ')]
+    if possible_nums(target, nums):
+        result += target
+
+print(result)
